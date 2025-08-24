4a0e3019d95788bba55e7e4ab19db73fee163664
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2025-08-24 16:55:15 -0400

    RED: test para agitar cacho  - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -5,3 +5,9 @@ def test_get_dice_values():
     dice_values = cacho.get_valor_dados()
     assert 0 <= len(dice_values) <= 5
 
+def test_agitar_cacho():
+    cacho = Cacho()
+    cacho.agitar_cacho()
+    dice_values = cacho.get_valor_dados()
+    dice_values_in_range = (1 <= x <= 6 for x in dice_values)
+    assert all(dice_values_in_range)
