2be22a9f1d80b196f80b1f9f52a785e255dbf216
YinYanCat <chyanez2023@udec.cl>
2025-08-23 22:48:48 -0400

    RED: test para [obtener denominación de los valores de los dados] - falla como esperado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.juego.dado import Dado
 def test_lanzar_dado(mocker):
     mock_randint = mocker.patch("src.juego.dado.random.randint", return_value = 6)
@@ -9,3 +11,9 @@ def test_get_valor():
     dado = Dado()
     dado.valor = 6
     assert dado.get_valor() == 6
+
+@pytest.mark.parametrize("test_input,expected",list(enumerate(['As','Tonto','Tren','Cuadra','Quina','Sexto'], start=1)))
+def test_denominacion_valor(test_input,expected):
+    dado = Dado()
+    dado.__valor = test_input
+    assert dado.denominacion_valor() == expected
