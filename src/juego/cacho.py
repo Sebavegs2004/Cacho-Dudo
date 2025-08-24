ce497887295efa9744f025b94570c2cc38cd4071
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2025-08-24 16:45:20 -0400

    GREEN: implementación mínima para obtener valores de los dados del cacho

---
+++ src/juego/cacho.py
@@ -0,0 +1,8 @@
+from src.juego.dado import Dado
+
+class Cacho:
+    def __init__(self):
+        self.dados = [Dado(), Dado(), Dado(), Dado(), Dado()]
+
+    def get_valor_dados(self):
+        return [x.get_valor() for x in self.dados]
