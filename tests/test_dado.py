586e39e016fb9ffbc6a52bcdd0c60cecb8775192
YinYanCat <chyanez2023@udec.cl>
2025-08-23 23:19:40 -0400

    [Fix]: tests no consideran saltar (bypass) encapsulamiento al modificar y leer variables privadas en unit tests.

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -5,15 +5,15 @@ def test_lanzar_dado(mocker):
     mock_randint = mocker.patch("src.juego.dado.random.randint", return_value = 6)
     dado = Dado()
     dado.lanzar_dado()
-    assert dado.__valor() == 6
+    assert dado._Dado__valor == 6
 
 def test_get_valor():
     dado = Dado()
-    dado.valor = 6
+    dado._Dado__valor = 6
     assert dado.get_valor() == 6
 
 @pytest.mark.parametrize("test_input,expected",list(enumerate(['As','Tonto','Tren','Cuadra','Quina','Sexto'], start=1)))
 def test_denominacion_valor(test_input,expected):
     dado = Dado()
-    dado.__valor = test_input
+    dado._Dado__valor = test_input
     assert dado.denominacion_valor() == expected
