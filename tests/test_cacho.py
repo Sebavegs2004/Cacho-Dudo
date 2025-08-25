fe707fd8526197f133cdb2d3270d323f73320ed7
4lehh <camposjavier143@gmail.com>
2025-08-24 23:02:49 -0400

    RED: test para devolver identificador - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -1,16 +1,20 @@
 from src.juego.cacho import Cacho
 
 def test_get_dice_values():
-    cacho = Cacho()
+    cacho = Cacho(1)
     dice_values = cacho.get_valor_dados()
     assert 0 <= len(dice_values) <= 5
 
 def test_get_denominaciones():
-    cacho = Cacho()
+    cacho = Cacho(1)
     denominacion = ['As', 'Tonto', 'Tren', 'Cuadra', 'Quina', 'Sexto']
     assert all(x in denominacion for x in cacho.get_denominacion_dados())
 
 def test_agitar_cacho():
-    cacho = Cacho()
+    cacho = Cacho(1)
     cacho.agitar_cacho()
     assert all(1 <= x <= 6 for x in cacho.get_valor_dados())
+
+def test_get_identificador_cacho():
+    cacho = Cacho(5)
+    assert cacho.get_indentificador() == 5
