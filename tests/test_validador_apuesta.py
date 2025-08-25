b18eee569c0a4a31534cc428d4f9a74e08170f78
4lehh <camposjavier143@gmail.com>
2025-08-24 22:08:53 -0400

    RED: test para validar apuesta inferior a la apuesta actual - falla como esperado

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -1,5 +1,19 @@
 from src.juego.validador_apuesta import ValidarApuesta
 
 def test_apuesta_as_sin_ronda_especial():
-    assert not ValidarApuesta.es_valida(apuesta_nueva=(1, 'As'), ronda=1, ronda_especial=False)
+    assert not ValidarApuesta.es_valida(apuesta_actual=(1, 'As'), apuesta_nueva=(1, 'As'), ronda=1, ronda_especial=False)
 
+def test_apuesta_inferior_al_anterior():
+    assert not ValidarApuesta.es_valida(apuesta_actual=(2, "tren"), apuesta_nueva=(2, "tonto"))
+
+# def test_apuesta_numero_a_as_valida():
+#     assert ValidarApuesta.es_valida((4, "tonto"), (3, "as"))
+
+# def test_apuesta_as_a_numero_valida():
+#     assert ValidarApuesta.es_valida((2, "as"), (5, "tonto"))
+
+# def test_apuesta_numero_a_as_invalida():
+#     assert not ValidarApuesta.es_valida((4, "tonto"), (2, "as"))
+
+# def test_apuesta_as_a_numero_invalida():
+#     assert ValidarApuesta.es_valida((2, "as"), (4, "tonto"))
