7c71f0817190b0b9d32b822f2b005569f2d1c29d
4lehh <camposjavier143@gmail.com>
2025-08-24 22:18:09 -0400

    RED: test para validar apuestas con as - falla como esperado

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -4,16 +4,9 @@ def test_apuesta_as_sin_ronda_especial():
     assert not ValidarApuesta.es_valida(apuesta_actual=(1, 'As'), apuesta_nueva=(1, 'As'), ronda=1, ronda_especial=False)
 
 def test_apuesta_inferior_al_anterior():
-    assert not ValidarApuesta.es_valida(apuesta_actual=(2, "tren"), apuesta_nueva=(2, "tonto"))
+    assert not ValidarApuesta.es_valida(apuesta_actual=(2, "tren"), apuesta_nueva=(2, "tonto"), ronda=10, ronda_especial=False)
 
-# def test_apuesta_numero_a_as_valida():
-#     assert ValidarApuesta.es_valida((4, "tonto"), (3, "as"))
+def test_apuesta_numero_a_as_valida():
+    assert ValidarApuesta.es_valida((4, "tonto"), (3, "as"))
 
-# def test_apuesta_as_a_numero_valida():
-#     assert ValidarApuesta.es_valida((2, "as"), (5, "tonto"))
 
-# def test_apuesta_numero_a_as_invalida():
-#     assert not ValidarApuesta.es_valida((4, "tonto"), (2, "as"))
-
-# def test_apuesta_as_a_numero_invalida():
-#     assert ValidarApuesta.es_valida((2, "as"), (4, "tonto"))
