69dec0cab25a92c7738fc58e68696c836fcabdaa
4lehh <camposjavier143@gmail.com>
2025-08-24 22:53:07 -0400

    REFACTOR: mejora de los test

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -6,11 +6,29 @@ def test_apuesta_as_sin_ronda_especial():
 def test_apuesta_inferior_al_anterior():
     assert not ValidarApuesta.es_valida(apuesta_actual=(2, "tren"), apuesta_nueva=(2, "tonto"), ronda=10, ronda_especial=False)
 
+def test_apuesta_inferior_al_anterior_invalida():
+    assert not ValidarApuesta.es_valida(apuesta_actual=(2, "tren"), apuesta_nueva=(2, "tonto"), ronda=10, ronda_especial=False)
+
 def test_apuesta_numero_a_as_valida():
     assert ValidarApuesta.es_valida(apuesta_actual=(4, "tonto"), apuesta_nueva=(3, "as"), ronda=10, ronda_especial=False)
+
+def test_apuesta_numero_a_as_invalida():
+    assert not ValidarApuesta.es_valida(apuesta_actual=(4, "tonto"), apuesta_nueva=(2, "as"), ronda=10, ronda_especial=False)
 
 def test_apuesta_as_a_numero_valida():
     assert ValidarApuesta.es_valida(apuesta_actual=(2, "as"), apuesta_nueva=(5, "tonto"), ronda=10, ronda_especial=False)
 
+def test_apuesta_as_a_numero_invalida():
+    assert not ValidarApuesta.es_valida(apuesta_actual=(2, "as"), apuesta_nueva=(4, "tonto"), ronda=10, ronda_especial=False)
+
 def test_apuesta_con_ases():
     assert ValidarApuesta.es_valida(apuesta_actual=(2, "as"), apuesta_nueva=(3, "as"), ronda=10, ronda_especial=False)
+
+def test_apuesta_con_ases_invalida():
+    assert not ValidarApuesta.es_valida(apuesta_actual=(2, "as"), apuesta_nueva=(2, "as"), ronda=10, ronda_especial=False)
+
+def test_apuesta_invalida_de_dados_invalida():
+    assert not ValidarApuesta.es_valida(apuesta_actual=(4, "tonto"), apuesta_nueva=(3, "tren"), ronda=10, ronda_especial=False)
+
+def test_apuesta_invalida_de_dados_valida():
+    assert ValidarApuesta.es_valida(apuesta_actual=(4, "tonto"), apuesta_nueva=(5, "tren"), ronda=10, ronda_especial=False)
