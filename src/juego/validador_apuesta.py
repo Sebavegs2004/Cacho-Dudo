0351819521fa9c31e8b92ca7c534b11717170fd0
4lehh <camposjavier143@gmail.com>
2025-08-24 22:16:39 -0400

    GREEN: implementacion minima para validar que apuesta nueva sea mayor a la apuesta actual

--- src/juego/validador_apuesta.py
+++ src/juego/validador_apuesta.py
@@ -5,6 +5,32 @@ class ValidarApuesta:
         # Partir ronda con As y sin ser ronda especial
         if apuesta_actual[1] == 'As' and ronda == 1 and not ronda_especial: 
             return False
-    
+
+
+        # Apuesta inferior por número sin contar AS
+        if apuesta_actual[0] > apuesta_nueva[0]:
+            return False
+        
+        # Apuesta igual por número sin contar AS
+        if apuesta_actual[0] == apuesta_nueva[0]:
+
+            # Validar que denominaciones sean validas
+            match apuesta_actual[1]:
+                case "tonto":
+                    if apuesta_nueva[1] in ["tonto"]:
+                        return False
+                case "tren":
+                    if apuesta_nueva[1] in ["tonto", "tren"]:
+                        return False
+                case "cuadra":
+                    if apuesta_nueva[1] in ["tonto", "tren", "cuadra"]:
+                        return False
+                case "quina":
+                    if apuesta_nueva[1] in ["tonto", "tren", "cuadra", "quina"]:
+                        return False
+                case "sexta":
+                    if apuesta_nueva[1] in ["tonto", "tren", "cuadra", "quina", "sexta"]:
+                        return False
+
         return True
 
