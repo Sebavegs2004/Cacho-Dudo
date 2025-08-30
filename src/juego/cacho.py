92df1736b93c1e205272570bc20d8f162a0624f4
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2025-08-30 18:57:07 -0400

    GREEN: implementación mínima para añadir dados a favor

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -4,6 +4,7 @@ class Cacho:
     def __init__(self, identificador: int = None):
         self.dados = [Dado(), Dado(), Dado(), Dado(), Dado()]
         self.identificador = identificador
+        self.dados_a_favor = 0
 
     def get_valor_dados(self):
         return [x.get_valor() for x in self.dados]
@@ -20,7 +21,10 @@ class Cacho:
             del self.dados[-1]
 
     def add_dado(self):
-        self.dados.append(Dado())
+        if len(self.dados) == 5:
+            self.dados_a_favor += 1
+        else:
+            self.dados.append(Dado())
     
     def get_cantidad_dados(self) -> int:
         return len(self.dados)
