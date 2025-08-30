1dc30e4a9f5b9134f41cdd15ffdcc649c8182511
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2025-08-30 19:20:06 -0400

    GREEN: implementación mínima para calzar exitosamente

---
+++ src/juego/arbitro_ronda.py
@@ -0,0 +1,17 @@
+from src.juego.contador_pintas import Contador_Pintas
+
+class Arbitro_Ronda:
+    def __init__(self):
+        self.contador_pintas = Contador_Pintas()
+        self.indices = {
+            "as": 0,
+            "tonto": 1,
+            "trenes": 2,
+            "cuadra": 3,
+            "quina": 4,
+            "sexta": 5
+        }
+
+    def calzar(self, jugador_calza, jugadores, apuesta_actual, ronda_especial):
+        if self.contador_pintas.contar_pintas(jugadores, ronda_especial)[self.indices[apuesta_actual[1]]] == apuesta_actual[0]:
+            jugador_calza.add_dado()
