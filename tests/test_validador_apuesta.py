c8af38c30a01a2ab9193128a66954ae9587caad9
4lehh <camposjavier143@gmail.com>
2025-08-30 15:28:58 -0400

    RED: test apuetsa con as en ronda especial - falla como esperado

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -1,7 +1,7 @@
 from src.juego.validador_apuesta import ValidarApuesta
 
 def test_apuesta_as_sin_ronda_especial():
-    assert not ValidarApuesta.es_valida(apuesta_actual=(1, 'As'), apuesta_nueva=(1, 'As'), ronda=1, ronda_especial=False)
+    assert not ValidarApuesta.es_valida(apuesta_actual=(1, 'as'), apuesta_nueva=(1, 'as'), ronda=1, ronda_especial=False)
 
 def test_apuesta_inferior_al_anterior():
     assert not ValidarApuesta.es_valida(apuesta_actual=(2, "tren"), apuesta_nueva=(2, "tonto"), ronda=10, ronda_especial=False)
@@ -32,3 +32,6 @@ def test_apuesta_invalida_de_dados_invalida():
 
 def test_apuesta_invalida_de_dados_valida():
     assert ValidarApuesta.es_valida(apuesta_actual=(4, "tonto"), apuesta_nueva=(5, "tren"), ronda=10, ronda_especial=False)
+
+def test_apuesta_as_ronda_especial():
+    assert ValidarApuesta.es_valida(apuesta_actual=(), apuesta_nueva=(1, "as"), ronda=1, ronda_especial=True)
