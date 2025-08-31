6fc54ff95c8f1adc779889fd0295db26cda5006d
4lehh <camposjavier143@gmail.com>
2025-08-31 17:45:17 -0400

    RED: test que regula el inicio de una partida - falla como esperado

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -3,3 +3,8 @@ from src.juego.gestor_partida import GestorPartida
 def test_gestor_partida_usuarios():
     gestor = GestorPartida(5)
     assert gestor.get_usuarios() == 5
+
+def test_iniciar_partida():
+    gestor = GestorPartida(5)
+    gestor.iniciar_partida()
+    assert gestor.get_usuarios() == 5
