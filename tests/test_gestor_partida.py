d6678610fc1b68896f734bd1b1937454f7e4ddad
4lehh <camposjavier143@gmail.com>
2025-08-31 17:47:55 -0400

    RED: test para el formateo de las apuestas dadas - falla como esperado

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -8,3 +8,10 @@ def test_iniciar_partida():
     gestor = GestorPartida(5)
     gestor.iniciar_partida()
     assert gestor.get_usuarios() == 5
+
+def test_formatear_apuesta():
+    gestor = GestorPartida(5)
+    assert gestor.formatear_apuesta("3 as") == (3, "as")
+    assert gestor.formatear_apuesta("0 tonto") == None
+    assert gestor.formatear_apuesta("7 tren") == None
+    assert gestor.formatear_apuesta("2 cuadra") == (2, "cuadra")
